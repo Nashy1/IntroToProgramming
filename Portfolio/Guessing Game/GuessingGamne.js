81bcaaf4f313d2d79701ab346ca108985b6f3cb5
Nashy1 <nashnyembe0902@gmail.com>
2023-07-13 10:15:24 +0200

    commit

---
+++ Portfolio/Guessing Game/GuessingGamne.js
@@ -0,0 +1,25 @@
+
+      var y = Math.floor(Math.random() * 10 -1);
+      var guess = 1;
+  
+      document.getElementById("submitguess").onclick = function(){
+  
+          
+          var x = document.getElementById("guessField").value;
+  
+          if(x == y)
+          {    
+            alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
+                  + guess + " GUESS ");
+          }
+          else if(x > y) /* if guessed number is greater than actual number*/
+          {    
+            guess++;
+            alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+          }
+          else
+          {
+            guess++;
+            alert("OOPS Sorry!! Try a Greater number")
+          }
+        }
---
+++ page.js
@@ -0,0 +1,89 @@
+// var fname = "Nickoooooo";
+// //  console.log ("Hello" + fname);
+// // if (fname.length > 7) {
+// //     console.log("Wow you have a REALLY long name!");
+// // } else {
+// //     console.log("Your name isnt very long.");
+// // }
+// let lemonChicken = true;
+// let beefWithBlackBean = false;
+// let sweetAndSourPork = true;
+// if (lemonChicken){
+//     alert("Grest Im haveing lemon Chicken");
+// } else if (beefWithBlackBean){
+//     alert("Im having the beef.");
+// }else if(sweetAndSourPork){
+//     alert("Ok Ill have the pork.");
+// } else {
+//     alert("Well, I guess Ill have rice then.");
+// } 
+// var fname = prompt("What is your name").toLowerCase();
+// const mom = "mom";
+// const dad = "dad";
+// const me = "nash";
+
+// if(fname=== "nash"){
+//     alert("Hello me");
+// } else if(fname ==="mom"){
+//     alert("Hello Mom");
+// } else if(fname==="papa"){
+//     alert("Hello Papa")
+// } else {
+//     alert("Hello stranger");
+// }
+// for (var sheepCounted = 0; sheepCounted < 100; sheepCounted++) {
+//     console.log("I have counted" +  sheepCounted  + "sheep!");
+// } console.log("Zzzzzzzz");
+ 
+// var timesToSayHello =3;
+// for(var i = 0; i < timesToSayHello; i++) {
+//     console.log("Hello!");
+// }
+// var animals =["Lion", "Flamingo","Poar Bear", "Boa Constrictor"];
+// for ( var i = 0; i< animals.length; i++);{
+//     console.log("This zoo contains a"+ animals[1] + ".");
+// }
+// var fname= "Nick";
+// for (var i =0; i< fname.length; i++){
+//     alert("My name contains the letter"+ fname[1]+ ".");
+// }
+//powers of 2
+// for (var x = 3; x < 10000; x= x * 3){
+//     console.log(x);
+// }
+// var x = 3;
+// while(x < 10000){
+//     console.log(x )
+//     x *= 3
+// } 
+// var countdown = 10;
+// for(var  countdown = 10; 0 < countdown; countdown--){
+//     console.log(countdown);
+    
+// }
+// var animals = [ "Cat", "Fish", "Lemur", "Komodo Dragon"]
+// for(var i = 0; i < animals.length; i++){
+//     console.log("Awesome " + animals[i] + ".")
+// }
+// var alphabet = "abcdefghiklmnopqrstuvwxyz";
+// var randomWord = "";
+// for (var i = 0; i< 7; i++){
+//     let randomNumber=Math.floor(Math.random()*27)
+//     randomWord = randomWord +(alphabet[randomNumber]);
+// } console.log(randomWord)
+// var input = "javascript is awesome";
+// var output = "";
+// for (var i=0; i<input.length; i++){
+// if(input[i] === "a"){
+//     output+= "4";
+// }else if (input[i] === "e"){
+//     output+= "3";
+// }else if( input[i] === "i"){
+//     output+= "1";
+// }else if(input[i] === "o"){
+//     output+="0";
+// }else{
+//     output+= input[i];
+// }
+// }
+// alert(output);
